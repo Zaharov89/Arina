fa744f7e05e75994b1495184b7c3f8a5fe45c417
Zaharov89 <70170730+Zaharov89@users.noreply.github.com>
2026-06-13 23:18:23 +0300

    Add future database model placeholders

---
+++ Arina/database/models.py
@@ -0,0 +1,12 @@
+"""Future SQLAlchemy models.
+
+Planned models:
+- User
+- StudentProfile
+- Subject
+- TestAttempt
+- TestAnswer
+- AccountActivationToken
+
+The concrete implementation will be added when PostgreSQL support starts.
+"""
