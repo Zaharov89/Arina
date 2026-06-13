302fc9397b7a9ec18fab57e55eea5d42b2ecf9e8
Zaharov89 <70170730+Zaharov89@users.noreply.github.com>
2026-06-13 23:17:32 +0300

    Move result routes to backend

---
+++ Arina/backend/routes/results.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template
+
+from Arina.backend.routes.common import get_student
+
+results_bp = Blueprint("results", __name__)
+
+
+@results_bp.route("/results/english")
+def results_english():
+    return render_template("results/english/results.html", student=get_student())
+
+
+@results_bp.route("/results/russian")
+def results_russian():
+    return render_template("results/russian/results.html", student=get_student())
+
+
+@results_bp.route("/results/math")
+def results_math():
+    return render_template("results/math/results.html", student=get_student())
