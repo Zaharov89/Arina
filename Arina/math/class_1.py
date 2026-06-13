d6c5feed56749c026eff4696e02d31934cc5d420
Zaharov89 <70170730+Zaharov89@users.noreply.github.com>
2026-06-13 23:52:57 +0300

    Extend class 1 math generator with topic tasks

--- Arina/math/class_1.py
+++ Arina/math/class_1.py
@@ -1,21 +1,34 @@
 import random
 
+from Arina.math.class_1_tasks import generate_class_1_topic_task
+from Arina.math.class_1_topics import CLASS_1_MATH_TOPICS
+
+
 class MathExamplesClass1:
     def __init__(self, example_type, table_num):
         self.type = example_type
         self.table_num = table_num
 
     def generate_example(self):
-        if self.type in ['all', 'addsub']:
-            op = random.choice(['+', '-'])
+        if self.type in CLASS_1_MATH_TOPICS:
+            return generate_class_1_topic_task(self.type)
+
+        if self.type == "addition_to_10":
+            return generate_class_1_topic_task("addition_to_10")
+
+        if self.type == "subtraction_to_10":
+            return generate_class_1_topic_task("subtraction_to_10")
+
+        if self.type in ["all", "addsub"]:
+            op = random.choice(["+", "-"])
         else:
             op = self.type
 
-        if op == '+':
+        if op == "+":
             # a, b от 0 до 20, но сумма строго <= 20
             a = random.randint(0, 20)
             b = random.randint(0, 20 - a)
-        elif op == '-':
+        elif op == "-":
             # уменьшаемое a от 0 до 20, вычитаемое b от 0 до a, чтобы результат ≥ 0
             a = random.randint(0, 20)
             b = random.randint(0, a)
@@ -24,8 +37,11 @@ class MathExamplesClass1:
             a = random.randint(0, 20)
             b = random.randint(0, 20)
 
-        return {'a': a, 'op': op, 'b': b}
+        return {"a": a, "op": op, "b": b}
 
     def calculate_answer(self, a, op, b):
-        if op == '+': return a + b
-        if op == '-': return a - b
+        if op == "+":
+            return a + b
+        if op == "-":
+            return a - b
+        return None
